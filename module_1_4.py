795ceb02afc8c59a286271e13fed3aa836709c12
KaRinaTrick <proxorova.karina36@gmail.com>
2024-11-23 09:24:53 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,5 @@
+home_tasks = 12
+hour_tasks = 1.5
+course = 'Python'
+hour_one_task = home_tasks / hour_tasks
+print (course+', всего задач:',home_tasks,', затрачено часов:',hour_tasks,', среднее время выполнения:',hour_one_task,'часа' )
