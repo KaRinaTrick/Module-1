7b4acd54503d8c40d9beb36628146589000d9e4a
KaRinaTrick <proxorova.karina36@gmail.com>
2024-11-23 09:28:29 +0300

    module_1_4.py

--- module_1_4.py
+++ module_1_4.py
@@ -1,5 +1,5 @@
 home_tasks = 12
 hour_tasks = 1.5
 course = 'Python'
-hour_one_task = home_tasks / hour_tasks
+hour_one_task = hour_tasks / home_task
 print (course+', всего задач:',home_tasks,', затрачено часов:',hour_tasks,', среднее время выполнения:',hour_one_task,'часа' )
